9a7f68e7c92be19db5a3e88a3ed24d4d72b60b75
Dev Raj <rehansingh.4522@gmail.com>
2021-12-25 16:24:20 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
